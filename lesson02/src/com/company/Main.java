b677ddfba29e510505d81df03160c8b3dc77fd55
Elad Lavi <eladlavi1@gmail.com>
2015-06-01 15:35:54 +0300

    lesson01

--- lesson02/src/com/company/Main.java
+++ lesson02/src/com/company/Main.java
@@ -3,7 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(123 / 10);
+        drawRectangle(50,6);
+
+
+    }
+    public static void exampleOfTrinaryIf(){
+        int x = 10;
+        /*
+        int y;
+        if(x<10)
+            y = 1;
+        else
+            y = 2;
+        */
+        //is the same as:
+        int y = x<10 ? 1 : 2;
     }
 
     // returns the distance between x and y, for example: distance(7,5) is 2
@@ -35,6 +49,8 @@ public class Main {
         return result;
     }
 
+    //how many whole times y fits in x
+    //for example quotient(7,3) is 2
     public static int quotient(int x, int y){
         if(y==0)
             return -1;
@@ -45,6 +61,7 @@ public class Main {
         return result;
     }
 
+    //remainder(7,3) is 1  3*2=6 7-6=1
     public static int remainder(int x, int y){
         if(y==0)
             return -1;
@@ -80,6 +97,8 @@ public class Main {
 
     }
 
+    //sums up the digits of a number
+    //for example: sumOfDigits(123) is 6
     public static int sumOfDigits(int x){
         int result = 0;
         while (x>0){
@@ -89,7 +108,44 @@ public class Main {
         return result;
     }
 
-    
+    //finds the highest digit of a number.
+    //for example largestDigit(123) is 3
+    public static int largestDigit(int x){
+        int result = 0;
+        while (x>0) {
+            int lastDigit = x % 10;
+            if (lastDigit > result)
+                result = lastDigit;
+            x /= 10;
+        }
+        return result;
+    }
+
+    //reverses the digits of a number.
+    //for example: reverseDigits(123) is 321
+    public static int reverseDigits(int x){
+        int result = 0;
+        while (x>0) {
+            int lastDigit = x % 10;
+            result *= 10;
+            result += lastDigit;
+            x /= 10;
+        }
+        return result;
+    }
+
+    public static void printX(int size){
+
+    }
+
+    public static void drawRectangle(int width, int height){
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print((i==0 || i==height-1 || j==0 || j==width-1) ? "*" : " ");
+            }
+            System.out.println();
+        }
+    }
 }
 /*
 power(3, 3)
